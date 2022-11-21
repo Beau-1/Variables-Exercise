fd06a27e9c9fa42d2e56f5bf9daf5d860ac89e4b
Beau <jamesbuchanan0528@gmail.com>
2022-11-21 10:58:43 -0600

    variable exercise about my dog

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿// Variable exercise
+
+
+// these are my variables
+
+
+string dogName = "Sparkey";
+
+int dogAge = 3;
+
+char symbol = '!';
+
+bool runs = true;
+
+double weight = 12.5;
+
+decimal vetBill = 300.4627m;
+
+// these are my interpalations 
+
+
+Console.WriteLine($"This is all about my dog!");
+Console.WriteLine($"My dog's name is {dogName}! and he is a good boy.");
+Console.WriteLine($"He's been around a while. He is {dogAge} years old.");
+Console.WriteLine($"His attitude is much like this symbole, {symbol}. He is just an excitable boy.");
+Console.WriteLine($"It is true that he likes to run. Can he run fast? {runs}");
+Console.WriteLine($"He isn't very big. He only weighs {weight}lbs.");
+Console.WriteLine($"One day he ran so much he hurt his little paw. The vet bill was, ${vetBill}!");
+Console.WriteLine("All and all he really is a good boy.");
